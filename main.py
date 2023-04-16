092b34deafd6a2dd01a4e9abb52621302b5e1796
Ulyana137Q <92953387+Ulyana137Q@users.noreply.github.com>
2023-04-16 17:07:08 +0500

    основа игры
    добавлены классы, спрайты, а так же их отрисовка

---
+++ main.py
@@ -0,0 +1,65 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+ #конструктор класса
+   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+       #вызываем конструктор класса (Sprite):
+       sprite.Sprite.__init__(self)
+       #каждый спрайт должен хранить свойство image - изображение
+       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+       self.speed = player_speed
+       #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+ #метод, отрисовывающий героя на окне
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+#класс главного игрока
+class Player(GameSprite):
+   #метод для управления спрайтом стрелками клавиатуры
+    def update_l(self):
+       keys = key.get_pressed()
+       if keys[K_w] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_s] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+    def update_r(self):
+       keys = key.get_pressed()
+       if keys[K_UP] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_DOWN] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+
+
+back = (200, 255, 255) #цвет фона (background)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+racket1 = Player('platform.jpg', 30, 200,10,50,10)
+racket2 = Player('platform.jpg',520,200,10,50,10)
+ball = GameSprite('tennis.png',200,200,50,50,10)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if not finish:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+    display.update()
+    clock.tick(FPS)
+
