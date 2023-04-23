5c3bf96a46c3409c7ce883d3dfb28bac813317e7
Ulyana137Q <92953387+Ulyana137Q@users.noreply.github.com>
2023-04-23 17:02:14 +0500

    add move
    добавили движение мяча, проигрыш/победу

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pygame import *
-# игра
+
 class GameSprite(sprite.Sprite):
  #конструктор класса
    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -44,6 +44,17 @@ racket1 = Player('platform.jpg', 30, 200,10,50,10)
 racket2 = Player('platform.jpg',520,200,10,50,10)
 ball = GameSprite('tennis.png',200,200,50,50,10)
 
+speed_x = 1
+speed_y = 1
+
+font.init()
+font = font.SysFont('veranda', 70)
+loseL = font.render('player L lose', True, (180,0,0))
+loseR = font.render('player R lose', True, (180,0,0))
+
+#font1 = font.SysFont('veranda', 20)
+res = font.render('R to reset', True, (180,0,0))
+
 game = True
 finish = False
 clock = time.Clock()
@@ -53,13 +64,36 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+        if e.type == KEYDOWN:
+            if e.key == K_r:
+                finish = False
+                ball.rect.x = 200
+                ball.rect.y = 200
+        
     if not finish:
         window.fill(back)
         racket1.update_l()
         racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if racket1.rect.colliderect(ball.rect) or racket2.rect.colliderect(ball.rect):
+            speed_x *= -1
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y *= -1
+        
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(loseL, (150,200))
+            window.blit(res, (150,win_height - 200))
+            
+           # game = False
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(loseR, (150,200))
+            #game = False
+
         racket1.reset()
         racket2.reset()
         ball.reset()
     display.update()
     clock.tick(FPS)
-
